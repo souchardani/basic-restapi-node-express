3ec3f6ec717b1be724bf37957d198023c97fb30e
souchardani <danielsou9782@gmail.com>
2024-04-16 13:53:49 +0200

    post add route added

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,9 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", (req,res) => {
-    
+    const user = req.body;
+    users.push(user)
+    res.send(`User with the name ${user.firstName} added to the database`)
 })
 
 export default router;
