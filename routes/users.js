31167ed6fd8ece5927c62ae5a2e11e9277c8d694
souchardani <danielsou9782@gmail.com>
2024-04-16 14:50:37 +0200

    unitl delete user done

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25
-    },
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24
-    }, 
-]
+const users = []
 
 //todas las rutas de aqui empiezan con /users
 router.get("/", (req, res) => {
@@ -22,8 +12,20 @@ router.get("/", (req, res) => {
 
 router.post("/", (req,res) => {
     const user = req.body;
-    users.push(user)
+    const userWithId = {...user, id: uuidv4()};
+    users.push(user);
     res.send(`User with the name ${user.firstName} added to the database`)
 })
 
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser)
+})
+
+router.delete("/:id", (req,res) => {
+    const {id} = req.params;
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with the id ${id} deleted from the database`);
+})
 export default router;
